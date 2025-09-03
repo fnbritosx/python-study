4d384ccf781015567617cd32d30a0f2624007d1d
fnbritosx <fnbritosx@gmail.com>
2025-09-03 16:23:38 -0300

    refactor: standardizes folder names with leading zeros

--- aula10-conditions/aula10.py
+++ aula10-conditions/aula10.py
@@ -1,6 +1,6 @@
-tempo = int(input('Quantos anos você tem? '))
+idade = int(input('Quantos anos você tem? '))
 
-if tempo >= 18:
+if idade >= 18:
     print('Você é maior de idade!')
 else:
     print('Você é menor de idade!')
